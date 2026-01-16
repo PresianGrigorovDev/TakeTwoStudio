96addfa1d36a2705eb412866760d176727dcea1b
Presian Grigorov <30540737+PresianGrigorovDev@users.noreply.github.com>
2026-01-17 01:58:58 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,18 +1,14 @@
 <?php
-// Позволява ни да виждаме грешките директно в браузъра
+// Изчистване на кеша и показване на грешки за тест
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-echo "--- Старт на Deployment ---<br>";
+echo "Initalizing deployment...<br>";
 
-// Опит за изпълнение на командата
-$output = shell_exec('/usr/local/cpanel/bin/jethost-git-sync --repo=TakeTwoStudio');
+// Изпълнява официалната команда на cPanel за Deploy
+// Заменете 'TakeTwoStudio' с точното име на репозиторито от cPanel
+$command = 'uapi VersionControl deployment create repository=TakeTwoStudio';
+$output = shell_exec($command . ' 2>&1');
 
-if ($output === null) {
-    echo "ГРЕШКА: Функцията shell_exec е забранена или командата не работи.<br>";
-} else {
-    echo "Резултат от сървъра: <pre>$output</pre>";
-}
-
-echo "--- Край ---";
+echo "Result: <pre>$output</pre>";
 ?>
