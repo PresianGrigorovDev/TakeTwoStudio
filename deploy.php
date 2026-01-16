14c8932e3b8891c1a7b104f78a7e054e6c99932b
Presian Grigorov <30540737+PresianGrigorovDev@users.noreply.github.com>
2026-01-17 01:53:18 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,5 +1,21 @@
 <?php
-// Изпълнява команда за обновяване на git хранилището
-shell_exec('/usr/local/cpanel/bin/jethost-git-sync --repo=TakeTwoStudio'); 
-echo "Deployment triggered!";
+// Позволява ни да виждаме грешките директно в браузъра
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "--- Старт на Deployment ---<br>";
+
+// Име на вашето хранилище (repository)
+$repo_name = "TakeTwoStudio";
+
+// Опит за изпълнение на командата
+$output = shell_exec("/usr/local/cpanel/bin/jethost-git-sync --repo=$repo_name 2>&1");
+
+if ($output === null) {
+    echo "ГРЕШКА: Функцията shell_exec е забранена или командата не работи.<br>";
+} else {
+    echo "Резултат от сървъра: <pre>$output</pre>";
+}
+
+echo "--- Край ---";
 ?>
