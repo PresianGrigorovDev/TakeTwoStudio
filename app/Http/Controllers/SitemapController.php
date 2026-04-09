0589f865d24cc49e54d222c1a87ad9c3be27ef0f
p.grigorov <30540737+PresianGrigorovDev@users.noreply.github.com>
2026-04-09 23:54:40 +0300

    Update SitemapController.php

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -13,6 +13,12 @@ class SitemapController extends Controller
             ['loc' => url('/baptism'),    'priority' => '0.90', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
             ['loc' => url('/commercial'),  'priority' => '0.90', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
             ['loc' => url('/graduation'), 'priority' => '0.90', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
+            ['loc' => url('/family'),        'priority' => '0.80', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
+            ['loc' => url('/portrait'),      'priority' => '0.80', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
+            ['loc' => url('/automotive'),    'priority' => '0.80', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
+            ['loc' => url('/architectural'), 'priority' => '0.80', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
+            ['loc' => url('/events'),        'priority' => '0.80', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
+            ['loc' => url('/booking'),       'priority' => '0.70', 'changefreq' => 'monthly', 'lastmod' => now()->format('Y-m-d')],
         ];
 
         return response()
